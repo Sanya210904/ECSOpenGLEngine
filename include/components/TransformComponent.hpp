516dd589b0f330cebdfbbd044e0018bba3b2564b
darkalit <tkachenkozen@gmail.com>
2023-10-02 19:19:42 +0300

    get transform with euler angles

--- include/components/TransformComponent.hpp
+++ include/components/TransformComponent.hpp
@@ -5,6 +5,7 @@
 
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/quaternion.hpp>
+#include <glm/gtx/euler_angles.hpp>
 
 struct TransformComponent
 {
@@ -16,10 +17,20 @@ struct TransformComponent
     
     glm::mat4 getTransform() const
     {
-        glm::mat4 rot = glm::toMat4(glm::quat(rotation));
-        return glm::translate(glm::mat4(1.0f), translation)
-                * rot
-                * glm::scale(glm::mat4(1.0f), scale);
+        glm::mat4 translation = glm::translate(glm::mat4(1.0f), this->translation);
+        glm::mat4 scale = glm::scale(glm::mat4(1.0f), this->scale);
+        glm::mat4 rotation = glm::yawPitchRoll(
+            this->rotation.x,
+            this->rotation.y,
+            this->rotation.z
+        );
+
+        return translation * rotation * scale;
+
+        // glm::mat4 rot = glm::toMat4(glm::quat(rotation));
+        // return glm::translate(glm::mat4(1.0f), translation)
+        //         * rot
+        //         * glm::scale(glm::mat4(1.0f), scale);
     }
 
     glm::vec3 translation {0.0f, 0.0f, 0.0f};
