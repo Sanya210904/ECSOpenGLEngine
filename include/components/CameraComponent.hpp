8a9f4e9e8797f968dddb41b65b6bd9ede2b624a5
darkalit <tkachenkozen@gmail.com>
2023-10-02 19:20:30 +0300

    Camera revamp

--- include/components/CameraComponent.hpp
+++ include/components/CameraComponent.hpp
@@ -1,15 +1,64 @@
 #pragma once
 
-#include <scene/Camera.hpp>
+#include <glm/glm.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
 
 struct CameraComponent
 {
     CameraComponent() = default;
     CameraComponent(const CameraComponent&) = default;
-    CameraComponent(const Camera& camera)
-        :
-        camera(camera) { }
+    ~CameraComponent() = default;
 
-    Camera camera;
-    bool fixedAspectRatio = false;
+    enum class ProjectionType
+    {
+        Perspective,
+        Orthographic
+    };
+
+    glm::mat4 getProj(float aspect) const
+    {
+        switch (projectionType)
+        {
+        case ProjectionType::Perspective:
+            return glm::perspective(glm::radians(fov), aspect, nearBound, farBound);
+
+        case ProjectionType::Orthographic:
+            return glm::ortho(-aspect, aspect, -1.0f, 1.0f, nearBound, farBound);
+        }
+    }
+
+    static glm::mat4 getView(const glm::mat4& tMat)
+    {
+        return glm::inverse(tMat);
+    }
+
+    static glm::vec3 getForward(const glm::vec3& rotation)
+    {
+        // return glm::normalize(glm::vec3(getView(tMat)[2])) * glm::vec3(1, 1, -1);
+
+        return glm::normalize(glm::vec3(
+            cos(rotation.x) * cos(rotation.y),
+            sin(rotation.y),
+            sin(rotation.x) * cos(rotation.y)
+        ));
+
+        // return glm::normalize(rotation);
+    }
+
+    static glm::vec3 getRight(const glm::mat4& tMat)
+    {
+        return glm::normalize(glm::vec3(getView(tMat)[0]));
+    }
+
+    static glm::vec3 getUp(const glm::mat4& tMat)
+    {
+        return glm::normalize(glm::vec3(getView(tMat)[1]));
+    }
+
+    float fov { 45.0f };
+    float nearBound { 0.001f };
+    float farBound { 100.0f };
+
+    ProjectionType projectionType { ProjectionType::Perspective };
 };
